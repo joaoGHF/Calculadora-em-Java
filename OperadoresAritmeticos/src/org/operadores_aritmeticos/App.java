424e5361fca0b746c60e219604c8342be9bc8f22
João Gabriel Horsth Fonseca <99850827+joaoGHF@users.noreply.github.com>
2023-08-11 00:19:05 +0000

    Convert run() to a boolean type

--- OperadoresAritmeticos/src/org/operadores_aritmeticos/App.java
+++ OperadoresAritmeticos/src/org/operadores_aritmeticos/App.java
@@ -5,13 +5,14 @@ public class App {
         run();
     }
 
-    private static void run() {
+    private static boolean run() {
         System.out.println("Select a option:\n" +
                 "\t1 - sum\n" +
                 "\t2 - sub\n" +
                 "\t3 - mul\n" +
                 "\t4 - div\n" +
-                "\t5 - pow\n");
+                "\t5 - pow\n" +
+                "\tOther num to close\n");
 
         long n1 = 0, n2 = 0;
         int op = (int) GetNum.getNum("option");
@@ -22,39 +23,38 @@ public class App {
                 n2 = GetNum.getNum("second");
 
                 System.out.println("Sum is: " + Calculadora.sum(n1, n2));
-                break;
-
+                return true;
             case 2:
                 n1 = GetNum.getNum("first");
                 n2 = GetNum.getNum("second");
 
                 System.out.println("Sub is: " + Calculadora.sub(n1, n2));
-                break;
+                return true;
 
             case 3:
                 n1 = GetNum.getNum("first");
                 n2 = GetNum.getNum("second");
 
                 System.out.println("Mul is: " + Calculadora.mul(n1, n2));
-
-                break;
+                return true;
 
             case 4:
                 n1 = GetNum.getNum("first");
                 n2 = GetNum.getNum("second");
 
                 System.out.println("Div is: " + Calculadora.div(n1, n2));
-                break;
+                return true;
 
             case 5:
                 n1 = GetNum.getNum("base");
                 n2 = GetNum.getNum("expo");
+
                 System.out.println("Pow is: " + Calculadora.pow(n1, n2));
-                break;
+                return true;
 
             default:
-                System.out.println("Invalid number");
-                break;
+                System.out.println("Closing...");
+                return false;
         }
     }
 }
